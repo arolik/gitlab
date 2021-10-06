4bf3cf5dce60a3f87f1cef7b75c2a6eaa6b5c5cc
arolik <43118063+arolik@users.noreply.github.com>
2021-10-06 20:56:06 +0300

    create filter

--- js/main.js
+++ js/main.js
@@ -5,3 +5,56 @@ $(document).ready(function(){
     });
     
 });
+
+class MennageGoods {
+    constructor(){
+        this.men = document.querySelector('#men');
+        this.women = document.querySelector('#women');
+        this.children = document.querySelector('#children'); 
+        this.showGoodsBtn = document.querySelector('#showGoods');
+        this.blockOfMen = document.querySelector('#goodsMen');
+        this.blockOfWomen = document.querySelector('#goodsWomen');
+        this.isShowMens = true;
+        this.isShowWomens = true;
+
+        this.men.addEventListener('change', this.hanldeMensGoods.bind(this));
+        this.women.addEventListener('change', this.handleWomensGoods.bind(this));
+        this.showGoodsBtn.addEventListener('click', this.applyFilter.bind(this));
+    }
+
+    hanldeMensGoods(event){
+        if(event.target.checked == false){
+            this.isShowMens = false;
+        } else if (event.target.checked == true){
+            this.isShowMens = true;
+        }
+    }
+    handleWomensGoods(event){
+        if(event.target.checked == false){
+            this.isShowWomens = false;
+        } else if (event.target.checked == true){
+            this.isShowWomens = true;
+        }
+    }
+    applyFilter(){
+        if(this.isShowMens == false){
+            this.blockOfMen.style = "display:none";
+        }
+        if(this.isShowMens == true){
+            this.blockOfMen.style = "display:flex";
+        }
+        if(this.isShowWomens == false){
+            this.blockOfWomen.style = "display:none";
+        }
+        if(this.isShowWomens == true){
+            this.blockOfWomen.style = "display:flex";
+        }
+    }
+}
+
+new MennageGoods();
+
+
+
+
+
